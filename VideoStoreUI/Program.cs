b0abede8f951524b8f463febb848cd04f11510ec
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-06-01 09:37:48 +0200

    Add initial UI menu

--- VideoStoreUI/Program.cs
+++ VideoStoreUI/Program.cs
@@ -11,6 +11,40 @@ namespace VideoStoreUI
         static void Main(string[] args)
         {
             // TODO
+            var quit = false;
+
+            do
+            {
+                Console.Clear();
+                PrintMenu();
+
+                Console.Write("> ");
+                var menuChoice = Console.ReadLine();
+
+                switch (menuChoice?.ToLower())
+                {
+                    case "q":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine($"{menuChoice} is not a valid alternative\n");
+                        break;
+                }
+                Console.ReadLine();
+            } while (!quit);
+        }
+
+        private static void PrintMenu()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Welcome to 'Video Store'");
+            sb.AppendLine("1. Register customer");
+            sb.AppendLine("2. Add new movie");
+            sb.AppendLine("3. Rent Movie(s)");
+            sb.AppendLine("4. Receive rented movie");
+            sb.AppendLine("q. Quit");
+
+            Console.WriteLine(sb);
         }
     }
 }
